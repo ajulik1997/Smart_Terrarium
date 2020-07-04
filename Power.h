3255d1d083ac0f68d6ee0a7c1a346bb7d09fbad6
ajulik1997 <alexadner.liptak.jr@googlemail.com>
2020-07-04 01:31:30 +0100

    Tab to Space conversion test

--- Power.h
+++ Power.h
@@ -1,22 +1,22 @@
 #ifndef _TERRARIUM_POWER_H_
 #define _TERRARIUM_POWER_H_
 
-#include "Arduino.h"				// Arduino core
-#include <Battery.h>				// Battery sense library
+#include "Arduino.h"                // Arduino core
+#include <Battery.h>                // Battery sense library
 
 class Power
 {
   public:
-    Power(byte SENSE);				// Default constructor
-    void update_status();			// Update battery percentage
-    void initial_update();			// Initial battery update
-    unsigned int percentage;		// Time-averaged battery percentage
+    Power(byte SENSE);              // Default constructor
+    void update_status();           // Update battery percentage
+    void initial_update();          // Initial battery update
+    unsigned int percentage;        // Time-averaged battery percentage
   private:
-    byte _sense_pin;				// Sense pin connected to battery
-    Battery _driver;				// Battery driver
-    unsigned int _tmp_percentage;	// Rolling battery percentage to be averaged
-    byte _poll_count;				// Number of times battery level has been polled
-    unsigned long _last_updated;	// Last time battery status was updated
+    byte _sense_pin;                // Sense pin connected to battery
+    Battery _driver;                // Battery driver
+    unsigned int _tmp_percentage;   // Rolling battery percentage to be averaged
+    byte _poll_count;               // Number of times battery level has been polled
+    unsigned long _last_updated;    // Last time battery status was updated
 };
 
 #endif
