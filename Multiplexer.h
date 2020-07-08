c8e010ddef69d7b8978b86d34b763269c94a4fcd
ajulik1997 <alexadner.liptak.jr@googlemail.com>
2020-07-09 00:06:07 +0100

    Removed VoltageReference library dependance

--- Multiplexer.h
+++ Multiplexer.h
@@ -9,15 +9,15 @@ class Multiplexer
   public:
     Multiplexer(byte SIG, byte EN, byte S0, byte S1, byte S2, byte S3);    // Default constructor
     int read_device(byte device_id);                                       // Reads analog value of device
-	void lock_enable();                                                    // Locks multiplexer in enabled mode
-	void unlock_enable();                                                  // Removes enable lock, allowing multiplexer to be disabled
-	void switch_to_device(byte device_id);                                 // Switches to device without reading from it
-	byte signal_pin;                                                       // Signal pin which receives analog data
-	byte enable_pin;                                                       // Enable pin, when driven high, disables multiplexer
+    void lock_enable();                                                    // Locks multiplexer in enabled mode
+    void unlock_enable();                                                  // Removes enable lock, allowing multiplexer to be disabled
+    void switch_to_device(byte device_id);                                 // Switches to device without reading from it
+    byte signal_pin;                                                       // Signal pin which receives analog data
+    byte enable_pin;                                                       // Enable pin, when driven high, disables multiplexer
   private:
     CD74HC4067 _driver;                                                    // Multiplexer driver for switching channels
-	int _analog_value = 0;                                                 // Stores analog input value of _signal_pin
-	bool _lock = false;                                                    // If lock is true, multiplexer cannot be disabled
+    int _analog_value = 0;                                                 // Stores analog input value of _signal_pin
+    bool _lock = false;                                                    // If lock is true, multiplexer cannot be disabled
 };
 
 #endif
--- Power.cpp
+++ Power.cpp
@@ -2,22 +2,21 @@
 #include "Multiplexer.h"        // Analog multiplexer library
 #include "Power.h"              // Corresponding header
 #include <Battery.h>            // Battery sense library
-#include <VoltageReference.h>   // Library for referencing real value of VCC pin
 
 Power::Power(Multiplexer *amux, byte amux_device) : _driver(3000, 4200, amux->signal_pin){
   // Constructor for power class
   // Multiplexer object is used to select battery sense pin amux_device read from it
   _amux = amux;
   _amux_device = amux_device;
-  _driver.begin(VoltageReference().readVcc(), 1, &sigmoidal);   // Initialise battery monitoring with respect to real VCC voltage
-  _amux->lock_enable();                                         // Lock multiplexer in enabled mode, as muliple readings will be made
-  _amux->switch_to_device(_amux_device);						// Switch multiplexer to device representing battery voltage pin
+  _driver.begin(4960, 1, &sigmoidal);       // Initialise battery monitoring with respect to 5V (measured as 4.96V at Vin between 10-13V)
+  _amux->lock_enable();                     // Lock multiplexer in enabled mode, as muliple readings will be made
+  _amux->switch_to_device(_amux_device);    // Switch multiplexer to device representing battery voltage pin
   
-  for (byte i = 0; i < 10; i++) {                               // Take 10 battery percentage readings
-	  _tmp_percentage += _driver.level();                       // Readings are added, so their average can be taken
-	  delay(100);												// Take readings every 0.1s
+  for (byte i = 0; i < 10; i++) {           // Take 10 battery percentage readings
+	  _tmp_percentage += _driver.level();   // Readings are added, so their average can be taken
+	  delay(100);                           // Take readings every 0.1s
   }
   
-  level = _tmp_percentage / 10;                                 // Obtain and store time-averaged battery percentage
-  _amux->unlock_enable();                                       // Unlock the multiplexer enable pin, so it can be switched off
+  level = _tmp_percentage / 10;             // Obtain and store time-averaged battery percentage
+  _amux->unlock_enable();                   // Unlock the multiplexer enable pin, so it can be switched off
 }
